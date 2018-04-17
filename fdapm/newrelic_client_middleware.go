33089946008b1d16412185d21881935068a3a733
Guilherme Santos <guilherme.santos@foodora.com>
2018-04-17 16:15:23 +0200

    Protect in case txn is nil

--- fdapm/newrelic_client_middleware.go
+++ fdapm/newrelic_client_middleware.go
@@ -9,9 +9,14 @@ import (
 
 // NewRelicClientMiddleware call fdhttp.Doer instrumenting with NewRelic
 func NewRelicClientMiddleware(c fdhttp.Doer, txn newrelic.Transaction, req *http.Request) (*http.Response, error) {
+	if txn == nil {
+		return c.Do(req)
+	}
+
 	s := newrelic.StartExternalSegment(txn, req)
 	resp, err := c.Do(req)
 	s.Response = resp
 	s.End()
+
 	return resp, err
 }
