fcc31c56fc36dae9cc1f3abfeb55e6290c1ccad9
Guilherme Santos <guilherme.santos@foodora.com>
2018-04-17 15:12:01 +0200

    Add parameter name

--- fdhttp/middleware.go
+++ fdhttp/middleware.go
@@ -3,4 +3,4 @@ package fdhttp
 import "net/http"
 
 // Middleware is the method signature used as a wrapper of http request
-type Middleware func(http.Handler) http.Handler
+type Middleware func(next http.Handler) http.Handler
