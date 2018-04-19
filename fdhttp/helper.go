f0d548db971e21d7071935573abf6493297fd2a3
Guilherme Santos <guilherme.santos@foodora.com>
2018-04-20 00:57:42 +0200

    In case 205 ignore body

--- fdhttp/helper.go
+++ fdhttp/helper.go
@@ -27,6 +27,10 @@ func ResponseJSON(w http.ResponseWriter, statusCode int, resp interface{}) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(statusCode)
 
+	if resp == nil {
+		return
+	}
+
 	if err := json.NewEncoder(w).Encode(resp); err != nil {
 		panic(err)
 	}
