6f636d5969a07bb21fab4df7a16697e91b818067
Guilherme Santos <guilherme.santos@foodora.com>
2018-04-30 18:44:10 +0200

    Improve backoff func and set SetConnMaxLifetime

--- fddb/sql.go
+++ fddb/sql.go
@@ -7,23 +7,48 @@ import (
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 )
 
 // MaxConnAttempt is the number of times that it'll try to connect to the database
 // in case of error. Use -1 to try forever and 0 to not try again
 var MaxConnAttempt = 5
 
-// MinBackoff is the first duration that it'll wait, after that we'll
-// wait for MinBackoff * pow(2, attempt). The default value will give you:
-// 5s, 10s, 20s, 40s, 1m20s, 2m40s
-var MinBackoff = 5 * time.Second
+// BackoffFunc is a generator of duration that we use to sleep
+// between attempt in case we cannot connect to database.
+// Default implementation use following formula:
+// MinBackoff * pow(2, attempt) will will result in 5s, 10s, 20s, 40s, 1m20s, 2m40s
+// You also can override with something like this:
+//
+// 		fddb.BackoffFunc = func() func(attempt int) time.Duration {
+// 			d := []time.Duration{
+// 				5*time.Second,
+// 				10*time.Second,
+// 				20*time.Second,
+// 				1*time.Minute + 20*time.Second,
+// 				2*time.Minute + 40*time.Second,
+// 			}
+// 			return func(attempt int) time.Duration {
+// 				if attempt > len(d) {
+// 					return d[len(d)-1]
+//				}
+// 				return d[attempt-1]
+// 			}
+// 		}
+var BackoffFunc = func() func(attempt int) time.Duration {
+	startBackoff := float64(5 * time.Second)
+
+	return func(attempt int) time.Duration {
+		return time.Duration(startBackoff * math.Pow(2, float64(attempt-1)))
+	}
+}
 
 // DefaultMaxOpenConnection call SetMaxOpenConns to limit the number of connection
 // because by default no limit is setted, you also can override it, calling:
 // db.SetMaxOpenConns(N)
 var DefaultMaxOpenConnection = 100
 
-// Open a connection with mysql using provide configuration
+// Open a connection with sql database using provide configuration
 func OpenSQL(c SQLConfig) (*sql.DB, error) {
 	c = c.DefaultConfig()
 	defaultLogger.Printf("Connecting to '%s'...", c.String())
@@ -34,29 +59,32 @@ func OpenSQL(c SQLConfig) (*sql.DB, error) {
 	}
 
 	db.SetMaxOpenConns(DefaultMaxOpenConnection)
+	db.SetConnMaxLifetime(1 * time.Hour)
 
 	var attempt int
+	sleepFn := BackoffFunc()
+
 	for {
 		err := db.Ping()
 		if err == nil {
 			break
 		}
 
-		var prefix string
+		attempt++
 
+		var prefix string
 		if MaxConnAttempt > -1 {
-			if attempt >= MaxConnAttempt {
+			if attempt > MaxConnAttempt {
 				return nil, fmt.Errorf("fddb: %s: cannot connect to '%s': %s", c.Driver, c, err)
 			}
 
-			prefix = fmt.Sprintf("[%d/%d] ", attempt+1, MaxConnAttempt)
+			prefix = fmt.Sprintf("[%d/%d] ", attempt, MaxConnAttempt)
 		}
 
-		sleepFor := time.Duration(float64(MinBackoff) * math.Pow(2, float64(attempt)))
+		sleepFor := sleepFn(attempt)
 		defaultLogger.Printf("%sCannot connect to database, trying again in %s: %s", prefix, sleepFor, err)
 		time.Sleep(sleepFor)
 
-		attempt++
 	}
 
 	return db, nil
