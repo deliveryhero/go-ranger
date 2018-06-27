cfe0e16252d276eae0dfd40073c84e843128a111
Guilherme Santos <guilherme.santos@foodora.com>
2018-06-27 10:54:01 +0200

    Improve dbconfig

--- fddb/config.go
+++ fddb/config.go
@@ -3,74 +3,126 @@ package fddb
 import (
 	"errors"
 	"fmt"
+	"strings"
 )
 
-type SQLConfig struct {
-	Driver   string
-	Host     string
-	Port     string
+type DBConfig struct {
+	// Driver for now can be mysql, postgres, mongodb or dynamodb
+	Driver string
+
+	// Host should be use together with Port to build the db address
+	Host string
+
+	// Port will be append to Host to build the db address
+	Port string
+
+	// Addrs should be use when you want to inform more than one
+	// host and port, but also will work with just one.
+	// If you specify Host, Port and Addrs we'll try to connect
+	// to all (host:port and each address informed in Addrs)
+	Addrs []string
+
 	User     string
 	Password string
 	DB       string
 }
 
-var availableDrivers = map[string]struct{}{
-	"mysql":    {},
-	"postgres": {},
+var availableDrivers = map[string]DBConfig{
+	"mysql":    DBConfig{Port: "3306"},
+	"postgres": DBConfig{Port: "5432"},
+	"mongodb":  DBConfig{Port: "27017"},
+	"redis":    DBConfig{Port: "6379"},
 }
 
-// ErrNoDriverSpecified is returned when you call fddb.OpenSQL without
+// ErrNoDriverSpecified will be panic when you call fddb.Open without
 // specificy the driver name.
-var ErrNoDriverSpecified = errors.New("fddb: sql driver was not specified")
+var ErrNoDriverSpecified = errors.New("fddb: driver was not specified")
 
-// ErrUnknownDriver is returned when you call fddb.OpenSQL specificing
+// ErrUnknownDriver will be panic when you call fddb.Open specificing
 // a unknown driver name.
-var ErrUnknownDriver = errors.New("fddb: sql driver unknown")
+var ErrUnknownDriver = errors.New("fddb: driver unknown")
+
+// ErrInvalidPort will be panic when you call fddb.Open without specify
+// a port and we don't have a default one.
+var ErrInvalidPort = errors.New("fddb: invalid port")
 
 // DefaultConfig return a new config filling with default values
 // field that was not provided.
-func (c SQLConfig) DefaultConfig() SQLConfig {
+func (c DBConfig) init() DBConfig {
 	if c.Driver == "" {
 		panic(ErrNoDriverSpecified)
 	}
-	if _, ok := availableDrivers[c.Driver]; !ok {
+
+	var (
+		defaultCfg DBConfig
+		ok         bool
+	)
+
+	if defaultCfg, ok = availableDrivers[c.Driver]; !ok {
 		panic(ErrUnknownDriver)
 	}
 
+	if len(c.Addrs) == 0 && c.Host == "" {
+		if defaultCfg.Host != "" {
+			c.Host = defaultCfg.Host
+		} else {
+			c.Host = "127.0.0.1"
+		}
+	}
+
+	if c.Host != "" && c.Port == "" {
+		if defaultCfg.Port != "" {
+			c.Port = defaultCfg.Port
+		} else {
+			panic(ErrInvalidPort)
+		}
+	}
+
 	if c.User == "" {
-		c.User = "root"
-	}
-	if c.Host == "" {
-		c.Host = "127.0.0.1"
-	}
-	if c.Port == "" {
-		switch c.Driver {
-		case "mysql":
-			c.Port = "3306"
-		case "postgres":
-			c.Port = "5432"
+		if defaultCfg.User != "" {
+			c.User = defaultCfg.User
+		} else {
+			c.User = "root"
 		}
 	}
 
 	return c
 }
 
-func (c SQLConfig) String() string {
+func (c DBConfig) String() string {
 	return fmt.Sprintf("%s@%s:%s/%s", c.User, c.Host, c.Port, c.DB)
 }
 
-func (c SQLConfig) ConnString() string {
+func (c DBConfig) ConnString() string {
 	usrPwd := c.User
 	if c.Password != "" {
 		usrPwd += ":" + c.Password
 	}
 
-	switch c.Driver {
-	case "mysql":
-		return fmt.Sprintf("%s@tcp(%s:%s)/%s", usrPwd, c.Host, c.Port, c.DB)
-	case "postgres":
-		return fmt.Sprintf("postgres://%s@%s:%s/%s", usrPwd, c.Host, c.Port, c.DB)
+	var host string
+	if c.Host != "" {
+		host = fmt.Sprintf("%s:%s", c.Host, c.Port)
 	}
 
-	return fmt.Sprintf("%s@%s:%s/%s", usrPwd, c.Host, c.Port, c.DB)
+	switch c.Driver {
+	case "mysql":
+		return fmt.Sprintf("%s@tcp(%s)/%s", usrPwd, host, c.DB)
+	case "redis":
+		return host
+	case "mongodb":
+		if len(c.Addrs) > 0 {
+			addrs := c.Addrs[:]
+			if host != "" {
+				addrs = append([]string{host}, addrs...)
+			}
+
+			host = strings.Join(addrs, ",")
+		}
+		fallthrough
+	default:
+		return fmt.Sprintf("%s://%s@%s/%s", c.Driver, usrPwd, host, c.DB)
+	}
+
+	// should never reach it
+	return ""
 }
--- fddb/config_test.go
+++ fddb/config_test.go
@@ -1,14 +1,126 @@
-package fddb_test
+package fddb
 
 import (
 	"testing"
 
-	"github.com/foodora/go-ranger/fddb"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSQLConfigString(t *testing.T) {
-	c := fddb.SQLConfig{
+func TestDBConfig(t *testing.T) {
+	testCases := map[string]struct {
+		cfg DBConfig
+		res string
+	}{
+		"mysql basic": {
+			cfg: DBConfig{Driver: "mysql"},
+			res: "root@tcp(127.0.0.1:3306)/",
+		},
+		"mysql complete": {
+			cfg: DBConfig{
+				Driver:   "mysql",
+				Host:     "mysql.local",
+				Port:     "4321",
+				User:     "foodora",
+				Password: "pd123",
+				DB:       "test",
+			},
+			res: "foodora:pd123@tcp(mysql.local:4321)/test",
+		},
+		"postgres basic": {
+			cfg: DBConfig{Driver: "postgres"},
+			res: "postgres://root@127.0.0.1:5432/",
+		},
+		"postgres complete": {
+			cfg: DBConfig{
+				Driver:   "postgres",
+				Host:     "postgres.local",
+				Port:     "4321",
+				User:     "foodora",
+				Password: "pd123",
+				DB:       "test",
+			},
+			res: "postgres://foodora:pd123@postgres.local:4321/test",
+		},
+		"mongodb basic": {
+			cfg: DBConfig{Driver: "mongodb"},
+			res: "mongodb://root@127.0.0.1:27017/",
+		},
+		"mongodb complete": {
+			cfg: DBConfig{
+				Driver:   "mongodb",
+				Host:     "mongodb.local",
+				Port:     "4321",
+				User:     "foodora",
+				Password: "pd123",
+				DB:       "test",
+			},
+			res: "mongodb://foodora:pd123@mongodb.local:4321/test",
+		},
+		"mongodb multiple addrs": {
+			cfg: DBConfig{
+				Driver: "mongodb",
+				Addrs: []string{
+					"master.mongodb.local:27017",
+					"slave1.mongodb.local:27017",
+					"slave2.mongodb.local:27017",
+				},
+				User: "foodora",
+				DB:   "test",
+			},
+			res: "mongodb://foodora@master.mongodb.local:27017,slave1.mongodb.local:27017,slave2.mongodb.local:27017/test",
+		},
+		"mongodb with host and address": {
+			cfg: DBConfig{
+				Driver: "mongodb",
+				Host:   "master.mongodb.local",
+				Addrs: []string{
+					"slave1.mongodb.local:27017",
+					"slave2.mongodb.local:27017",
+				},
+				User:     "foodora",
+				Password: "pd123",
+				DB:       "test",
+			},
+			res: "mongodb://foodora:pd123@master.mongodb.local:27017,slave1.mongodb.local:27017,slave2.mongodb.local:27017/test",
+		},
+		"redis basic": {
+			cfg: DBConfig{Driver: "redis"},
+			res: "127.0.0.1:6379",
+		},
+		"redis complete": {
+			cfg: DBConfig{
+				Driver:   "redis",
+				Host:     "redis.remove.host",
+				Port:     "4321",
+				User:     "foodora",
+				Password: "pd123",
+				DB:       "test",
+			},
+			res: "redis.remove.host:4321",
+		},
+	}
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, tc.res, tc.cfg.init().ConnString())
+		})
+	}
+
+}
+
+func TestDBConfig_NoDriver(t *testing.T) {
+	assert.Panics(t, func() {
+		DBConfig{}.init()
+	})
+}
+
+func TestDBConfig_InvalidDriver(t *testing.T) {
+	assert.Panics(t, func() {
+		DBConfig{Driver: "invalid"}.init()
+	})
+}
+
+func TestDBConfigString(t *testing.T) {
+	c := DBConfig{
 		Driver:   "mysql",
 		Host:     "127.0.0.1",
 		Port:     "3306",
@@ -20,8 +132,8 @@ func TestSQLConfigString(t *testing.T) {
 	assert.Equal(t, c.String(), "root@127.0.0.1:3306/test")
 }
 
-func TestSQLConfigConnString_MySQL(t *testing.T) {
-	c := fddb.SQLConfig{
+func TestDBConfigConnString_MySQL(t *testing.T) {
+	c := DBConfig{
 		Driver: "mysql",
 		Host:   "127.0.0.1",
 		Port:   "3306",
@@ -34,16 +146,44 @@ func TestSQLConfigConnString_MySQL(t *testing.T) {
 	assert.Equal(t, c.ConnString(), "root:r007@tcp(127.0.0.1:3306)/test")
 }
 
-func TestSQLConfigConnString_Postgres(t *testing.T) {
-	c := fddb.SQLConfig{
+func TestDBConfigConnString_Postgres(t *testing.T) {
+	c := DBConfig{
 		Driver: "postgres",
 		Host:   "127.0.0.1",
-		Port:   "3306",
+		Port:   "5432",
 		User:   "root",
 		DB:     "test",
 	}
-	assert.Equal(t, c.ConnString(), "postgres://root@127.0.0.1:3306/test")
+	assert.Equal(t, c.ConnString(), "postgres://root@127.0.0.1:5432/test")
 
 	c.Password = "r007"
-	assert.Equal(t, c.ConnString(), "postgres://root:r007@127.0.0.1:3306/test")
+	assert.Equal(t, c.ConnString(), "postgres://root:r007@127.0.0.1:5432/test")
+}
+
+func TestDBConfigConnString_MongoDB(t *testing.T) {
+	c := DBConfig{
+		Driver: "mongodb",
+		Host:   "127.0.0.1",
+		Port:   "27017",
+		User:   "root",
+		DB:     "test",
+	}
+	assert.Equal(t, c.ConnString(), "mongodb://root@127.0.0.1:27017/test")
+
+	c.Password = "r007"
+	assert.Equal(t, c.ConnString(), "mongodb://root:r007@127.0.0.1:27017/test")
+}
+
+func TestDBConfigConnString_Redis(t *testing.T) {
+	c := DBConfig{
+		Driver: "redis",
+		Host:   "127.0.0.1",
+		Port:   "6379",
+		User:   "root",
+		DB:     "test",
+	}
+	assert.Equal(t, c.ConnString(), "127.0.0.1:6379")
+
+	c.Password = "r007"
+	assert.Equal(t, c.ConnString(), "127.0.0.1:6379")
 }
