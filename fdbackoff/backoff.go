3fda059a6cbf92cd6926967e22f1acef0fddfc87
Guilherme Santos <guilherme.santos@foodora.com>
2018-07-11 15:16:49 +0200

    Return 0 when attempt is 0

--- fdbackoff/backoff.go
+++ fdbackoff/backoff.go
@@ -5,11 +5,19 @@ import (
 	"time"
 )
 
-// Func ...
+// Func return to the caller the duration that need to
+// wait between the next call.
+// All implementation return 0 in case attempt == 0, so
+// it makes sense start call your backoff func only after
+// the first unsuccessful attempt
 type Func func(attempt int) time.Duration
 
 var Fixed = func(waitFor time.Duration) Func {
-	return func(int) time.Duration {
+	return func(attempt int) time.Duration {
+		if attempt == 0 {
+			return 0
+		}
+
 		return waitFor
 	}
 }
@@ -17,13 +25,17 @@ var Fixed = func(waitFor time.Duration) Func {
 var Linear = func(startWith time.Duration) Func {
 	s := float64(startWith)
 	return func(attempt int) time.Duration {
-		return time.Duration(s * float64(attempt+1))
+		return time.Duration(s * float64(attempt))
 	}
 }
 
 var Exponential = func(startWith time.Duration) Func {
 	s := float64(startWith)
 	return func(attempt int) time.Duration {
-		return time.Duration(s * math.Pow(2, float64(attempt)))
+		if attempt == 0 {
+			return 0
+		}
+
+		return time.Duration(s * math.Pow(2, float64(attempt-1)))
 	}
 }
--- fdbackoff/backoff_test.go
+++ fdbackoff/backoff_test.go
@@ -11,23 +11,26 @@ import (
 func TestFixed(t *testing.T) {
 	fixedBackoff := fdbackoff.Fixed(2 * time.Second)
 
-	assert.Equal(t, 2*time.Second, fixedBackoff(0))
+	assert.Equal(t, 0*time.Second, fixedBackoff(0))
 	assert.Equal(t, 2*time.Second, fixedBackoff(1))
 	assert.Equal(t, 2*time.Second, fixedBackoff(2))
+	assert.Equal(t, 2*time.Second, fixedBackoff(3))
 }
 
 func TestLinear(t *testing.T) {
 	linearBackoff := fdbackoff.Linear(2 * time.Second)
 
-	assert.Equal(t, 2*time.Second, linearBackoff(0))
-	assert.Equal(t, 4*time.Second, linearBackoff(1))
-	assert.Equal(t, 6*time.Second, linearBackoff(2))
+	assert.Equal(t, 0*time.Second, linearBackoff(0))
+	assert.Equal(t, 2*time.Second, linearBackoff(1))
+	assert.Equal(t, 4*time.Second, linearBackoff(2))
+	assert.Equal(t, 6*time.Second, linearBackoff(3))
 }
 
 func TestExponential(t *testing.T) {
 	expBackoff := fdbackoff.Exponential(2 * time.Second)
 
-	assert.Equal(t, 2*time.Second, expBackoff(0))
-	assert.Equal(t, 4*time.Second, expBackoff(1))
-	assert.Equal(t, 8*time.Second, expBackoff(2))
+	assert.Equal(t, 0*time.Second, expBackoff(0))
+	assert.Equal(t, 2*time.Second, expBackoff(1))
+	assert.Equal(t, 4*time.Second, expBackoff(2))
+	assert.Equal(t, 8*time.Second, expBackoff(3))
 }
