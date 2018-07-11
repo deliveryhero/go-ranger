8d888189eb84f15548f737ef7c15b169268a79da
Guilherme Santos <guilherme.santos@foodora.com>
2018-07-11 16:06:55 +0200

    Implement client middleware to retry error calls

--- fdhttp/client_middleware.go
+++ fdhttp/client_middleware.go
@@ -1,6 +1,11 @@
 package fdhttp
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+
+	"github.com/foodora/go-ranger/fdbackoff"
+)
 
 // ClientMiddleware will wrap all Do calls
 type ClientMiddleware func(next Doer) Doer
@@ -16,4 +21,25 @@ func (f DoerFunc) Do(req *http.Request) (*http.Response, error) {
 // Doer specify a interface to http calls
 type Doer interface {
 	Do(req *http.Request) (*http.Response, error)
+}
+
+// NewClientRetryMiddleware will retry maxRetries using backoffFunc to wait between
+// these calls. Once we have a successful call, status code less than 500 we'll stop.
+// Status code 429 - Too Many Request will trigger a retry as well.
+func NewClientRetryMiddleware(maxRetries int, backoffFunc fdbackoff.Func) ClientMiddleware {
+	return func(next Doer) Doer {
+		return DoerFunc(func(req *http.Request) (resp *http.Response, err error) {
+			for retry := 0; retry < maxRetries; retry++ {
+				resp, err = next.Do(req)
+				if err == nil && resp.StatusCode < 500 && resp.StatusCode != http.StatusTooManyRequests {
+					// we can consider this situation as a successful call, let's return
+					return
+				}
+
+				time.Sleep(backoffFunc(retry + 1))
+			}
+
+			return
+		})
+	}
 }
--- fdhttp/client_middleware_test.go
+++ fdhttp/client_middleware_test.go
@@ -2,10 +2,14 @@ package fdhttp_test
 
 import (
 	"bytes"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
+	"github.com/foodora/go-ranger/fdbackoff"
 	"github.com/foodora/go-ranger/fdhttp"
 	"github.com/stretchr/testify/assert"
 )
@@ -39,4 +43,40 @@ func TestClient_CallMiddleware(t *testing.T) {
 
 	assert.True(t, called)
 	assert.True(t, srvCalled)
+}
+
+func TestClientRetryMiddleware(t *testing.T) {
+	middleware := fdhttp.NewClientRetryMiddleware(4, fdbackoff.Fixed(time.Millisecond))
+
+	c := fdhttp.NewClient()
+	c.Use(middleware)
+
+	var srvCalled int
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		srvCalled++
+
+		var status int
+		switch srvCalled {
+		case 1:
+			status = http.StatusInternalServerError
+		case 2:
+			status = http.StatusTooManyRequests
+		case 3:
+			status = http.StatusOK
+		default:
+			t.Error(t, "It was not expected this call")
+			status = http.StatusInternalServerError
+		}
+
+		w.WriteHeader(status)
+		fmt.Fprint(w, http.StatusText(status))
+	}))
+	defer ts.Close()
+
+	resp, err := c.Get(ts.URL)
+	assert.NoError(t, err)
+	assert.Equal(t, 3, srvCalled)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	assert.Equal(t, "OK", string(body))
 }
