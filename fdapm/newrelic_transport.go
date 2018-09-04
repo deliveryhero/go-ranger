8b4b388cbe73096bfe7ead0baf3329df03a11ba6
Guilherme Santos <guilherme.santos@foodora.com>
2018-09-04 17:44:40 +0200

    Implement NewRelicTransport as client middleware

--- fdapm/newrelic_transport.go
+++ fdapm/newrelic_transport.go
@@ -4,29 +4,54 @@ import (
 	"net/http"
 
 	"github.com/foodora/go-ranger/fdhttp/fdmiddleware"
+
 	newrelic "github.com/newrelic/go-agent"
 )
 
-// NewRelicClientMiddleware call fdhttp.Doer instrumenting with NewRelic
-// The basic way to use this http client middleware is:
-// 		req := http.NewRequest(http.MethodGet, "www.google.com", nil)
+// NewRelicTransport return a fdmiddleware.ClientMiddleware to instrument with NewRelic
+// your http calls.
+// The basic way to use is:
+// 		req := http.NewRequest(http.MethodGet, "http://www.foodora.de", nil)
 //
 // 		// txn can be a transaction that you create by yourself, or
-// 		// if you're using fdapm.NewRelicMiddleware in your server it'll be your
-// 		// w http.ResponseWriter (that you receive in your handler)
-//		// but if you implemented your handler receiving only ctx context.Context
+// 		// if you're using fdapm.NewRelicMiddleware in your http server it'll be your
+// 		// w http.ResponseWriter (that you receive in your handler).
+//		// Also if you implement your handler as fdhttp.EndpointFunc (receiving only ctx context.Context)
 //		// you can get it like this:
-// 		// txn := NewRelicTransaction(ctx)
-// 		resp, err := fdapm.NewRelicClientMiddleware(http.DefaultClient, txn, req)
-func NewRelicClientMiddleware(c fdmiddleware.Doer, txn newrelic.Transaction, req *http.Request) (*http.Response, error) {
-	if txn == nil {
-		return c.Do(req)
-	}
+// 		txn := fdapm.NewRelicTransaction(ctx)
+//  	httpClient := &http.Client{}
+// 		resp, err := fdapm.NewRelicTransport(httpClient, txn, req)
+//
+// If you have a global http.Client that you're reusing between different request,
+// see fdapm.NewRelicClientMiddleware.
+func NewRelicTransport(txn newrelic.Transaction) fdmiddleware.ClientMiddleware {
+	return fdmiddleware.ClientMiddlewareFunc(func(next http.RoundTripper) http.RoundTripper {
+		return fdmiddleware.RoundTripperFunc(func(req *http.Request) (*http.Response, error) {
+			if txn == nil {
+				return next.RoundTrip(req)
+			}
 
-	s := newrelic.StartExternalSegment(txn, req)
-	resp, err := c.Do(req)
-	s.Response = resp
-	s.End()
+			seg := newrelic.StartExternalSegment(txn, req)
+			resp, err := next.RoundTrip(req)
+			seg.Response = resp
+			seg.End()
 
-	return resp, err
+			return resp, err
+		})
+	})
+}
+
+// NewRelicClientMiddleware makes the http call instrumenting with newrelic.Transaction.
+// It's common that you have only one http.Client and you share it across multiple requests or
+// different parts of your application. The problem is if you're calling a external service
+// for each request you cannot add the transport using fdapm.NewRelicTransport to your
+// http client otherwise you're reusing the same transaction for different calls.
+// This method use the http.Client that you provided and add the middleware on it, only to execute
+// this call. Doing that, the transport is used only in a single request, and next calls will not
+// have the middleware anymore.
+func NewRelicClientMiddleware(httpClient *http.Client, txn newrelic.Transaction, req *http.Request) (*http.Response, error) {
+	c := &http.Client{}
+	*c = *httpClient
+	c.Transport = NewRelicTransport(txn).Wrap(c.Transport)
+	return c.Do(req)
 }
