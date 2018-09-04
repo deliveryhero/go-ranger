26172462620c4f70e60a36ce302c8cc858376dad
Guilherme Santos <guilherme.santos@foodora.com>
2018-09-04 17:58:17 +0200

    Rename property to make nicer use it

--- fdhttp/client.go
+++ fdhttp/client.go
@@ -20,7 +20,7 @@ type Client interface {
 // Client is a wrap to http.Client where you can add different configurations, like
 // fdhttp.WithFallback(), fdhttp.WithBackoff(), etc.
 type ClientImpl struct {
-	http.Client
+	GoClient *http.Client
 }
 
 // DefaultClientTimeout will be used when create a new Client
@@ -29,7 +29,7 @@ var DefaultClientTimeout = 10 * time.Second
 // NewClient return a new instace of fdhttp.Client
 func NewClient() *ClientImpl {
 	return &ClientImpl{
-		Client: http.Client{
+		GoClient: &http.Client{
 			Timeout:   DefaultClientTimeout,
 			Transport: http.DefaultTransport,
 		},
@@ -39,6 +39,31 @@ func NewClient() *ClientImpl {
 // Use a middleware to wrap all http calls
 func (c *ClientImpl) Use(middlewares ...fdmiddleware.ClientMiddleware) {
 	for _, m := range middlewares {
-		c.Client.Transport = m.Wrap(c.Client.Transport)
+		c.GoClient.Transport = m.Wrap(c.GoClient.Transport)
 	}
+}
+
+// Do is implementated to satisfy the fdhttp.Client interface.
+func (c *ClientImpl) Do(req *http.Request) (*http.Response, error) {
+	return c.GoClient.Do(req)
+}
+
+// Get is implementated to satisfy the fdhttp.Client interface.
+func (c *ClientImpl) Get(url string) (*http.Response, error) {
+	return c.GoClient.Get(url)
+}
+
+// Head is implementated to satisfy the fdhttp.Client interface.
+func (c *ClientImpl) Head(url string) (*http.Response, error) {
+	return c.GoClient.Head(url)
+}
+
+// Post is implementated to satisfy the fdhttp.Client interface.
+func (c *ClientImpl) Post(url string, contentType string, body io.Reader) (*http.Response, error) {
+	return c.GoClient.Post(url, contentType, body)
+}
+
+// PostForm is implementated to satisfy the fdhttp.Client interface.
+func (c *ClientImpl) PostForm(url string, data url.Values) (*http.Response, error) {
+	return c.GoClient.PostForm(url, data)
 }
